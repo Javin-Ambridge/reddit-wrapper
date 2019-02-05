2f115eecfb457f68d20f2e69ac642c0611ba3ebd
Javin-Ambridge <javin.ambridge@gmail.com>
2019-02-05 10:48:57 -0500

    Updated Readme + Updated default user agent

--- reddit-wrapper.js
+++ reddit-wrapper.js
@@ -2,7 +2,7 @@ var Api = require('./api');
 
 module.exports = function(options) {
 	// If user agent isn't defined, define it.
-	options.user_agent = options.user_agent || "Reddit-Snooper-V2";
+	options.user_agent = options.user_agent || "Reddit-Watcher-V2";
 
 	const api = Api(options);
 
